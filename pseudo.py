79bebc0f98b2f081fc709c1bb02c6cc18670ed02
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2024-07-06 00:01:30 +0530

    Update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -7,7 +7,7 @@ INSTRUCTIONS
 2. Put the file you want to convert into the same folder as it, and rename it to "py_file.py"
 3. Add a "#F" comment to any lines in the code which have a function call that doesn't assign anything (so no =),
 as the program cannot handle these convincingly
-4. Run the converter file
+4. Run the converter files
 '''
 
 python_file = 'speed_check.py'
